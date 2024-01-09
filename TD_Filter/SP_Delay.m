1791b970e89f064628fdea771bd1996b0cbadfc2
(CH3)2CO <32687661+c3h899@users.noreply.github.com>
2024-01-09 09:52:16 -0600

    Add files via upload

---
+++ TD_Filter/SP_Delay.m
@@ -0,0 +1,30 @@
+classdef SP_Delay < TD_Filter
+	% Provides a Time Shift Function using offset of the T Axis
+	% This Model preserves samples, and monotonic ordering data
+	% Correspondence between time axis of multiple records is not guarenteed
+	properties
+		Delay;
+	end
+	methods
+		function Obj = SP_FD_Compensation(Name, Desc, Delay)
+			% Populates filter with relevant data
+			Obj.Name = sprintf('(Delay) %s', Name);
+			Obj.Desc = sprintf('(Delay) %s', Desc);
+			Obj.FD_Model = FD_Model;
+		end
+		function [Y, Tp] = process(obj, T, X)
+			% Apply Time Delay
+			if( ~isempty(obj.Delay) )
+				Tp = T - Delay;
+				if( any(isnan(Tp)) )
+					error('SP_Delay: Time Shift Failed to Apply');
+					warning('SP_Delay: Time Shift Failed; Reverting');
+					Tp = T;
+				end
+			else
+				warning('SP_Delay: No Delay Specified');
+			end
+			Y = X;
+		end
+	end
+end
